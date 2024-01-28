083ca1acb082595aafeecd7ad4a3dbfde8b5c274
Sebastian Talarowski <talarpopek@gmail.com>
2024-01-28 21:54:23 +0100

    Push to master

--- src/test/java/org/tMobile/RunCucumberTest.java
+++ src/test/java/org/tMobile/RunCucumberTest.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/org/features",
-        plugin = {"pretty", "html:target/cucumber-reports"})
+        plugin = {"pretty", "html:target/cucumber-reports", "json:target/cucumber.json"})
 public class RunCucumberTest {
 }
