61baa7651aadceac9cb511aa5c9bef5d8f54df49
Sebastian Talarowski <talarpopek@gmail.com>
2024-01-29 15:41:46 +0100

    Push to master

--- src/test/java/org/tMobile/pages/basket/BasketPage.java
+++ src/test/java/org/tMobile/pages/basket/BasketPage.java
@@ -4,8 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.tMobile.pages.withoutSubscription.WithoutSubscriptionPage;
-import org.tMobile.utils.DriverFactory;
+
 
 public class BasketPage {
 
